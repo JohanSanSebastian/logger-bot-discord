9cf8f8bc6e1c0ba1a4439e22a50a607b1908910e
meee-low <30362944+meee-low@users.noreply.github.com>
2018-04-11 12:01:38 -0300

    Update logger_bot.py

--- logger_bot.py
+++ logger_bot.py
@@ -34,7 +34,6 @@ async def on_message_edit(before, after):
     if before.content == after.content: #guarantees that it doesn't trigger on embeds
         return None
     now = timestamp_now()
-    author = before.author
     channel = before.channel
     change = 'EDIT: [{} UTC] in #{}:\nbefore: {}\nafter:  {}\n\n'.format(
               now, channel, message_log_format(before), message_log_format(after)
@@ -56,12 +55,16 @@ def message_log_format(message):
     """How the messages are formatted in the logs.
        discord.Message -> str"""
     author = message.author.display_name
+    if author.isalnum():
+       clean_author = author
+    else:
+       clean_author = ''.join(c for c in author if c.isalnum())
     author_id = message.author.id #author id is useful in case the user changes their name.
     channel = message.channel
     timestamp = message.timestamp #a datetime.datetime object
     content = message.content
     f_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-    return '[%s UTC] %s in #%s: %s (USER_ID: %s)' % (f_timestamp, author, channel, content, author_id)
+    return '[%s UTC] %s in #%s: %s (USER_ID: %s)' % (f_timestamp, clean_author, channel, content, author_id)
     
 def timestamp_now():
     """Returns string of the current UTC timestamp in year-month-day hour:minute:second format."""
@@ -70,5 +73,5 @@ def timestamp_now():
     return now_formatted
 
 
-    
+
 client.run(token)
